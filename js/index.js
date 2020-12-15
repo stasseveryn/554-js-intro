c858de18a10aa0af899a6eb65af2355c0784d54b
stasseveryn <stasseveryn@gmail.com>
2020-12-15 15:39:15 +0200

    new

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//alert('Hello JavaScript');
+
+let message = 'Hello World';
+//alert(message);
+message = 'Nice to see you';
+//alert(message);
+
+let num = 42;
+num = num + 1;
+//alert(num);
+
+const age = prompt('Enter you age');
+
+
+
